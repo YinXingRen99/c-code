94e72c5c6fa88a76e01b2213e61ed9235fa2408a
qq-3241820257 <77088501+qq-3241820257@users.noreply.github.com>
2021-02-25 15:23:24 +0800

    1

---
+++ 第二章/第二章/2.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define NAME "Song zeng"
+#define ADDRESS "湖北省襄阳市襄州区罗庄村二组"
+#define DAY_PER_YEAR 365
+//void jolly()
+//{
+//	printf("Brazill, ");
+//	/*int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("For,he's is a jolly good fellow!\n");
+//	}*/
+//}
+//void deny()
+//{
+//	printf("India, Chain\n");
+//	/*printf("Which nobody can deny!\n");*/
+//}
+//int main()
+//{ 
+//	jolly();
+//	printf("Russia, ");
+//	deny();
+//	deny();
+//	jolly();
+//	printf("Russia");
+//
+//	/*int age, days;
+//	age = 19;
+//	days = age * DAY_PER_YEAR;
+//	printf("You age is %d, and It is %d days\n", age, days);*/
+//	//printf("%s\n", NAME);
+//	//printf("%s\n", ADDRESS);
+//	//	/*printf("Song zeng\n");
+//	//	printf("Song\nzeng\n");
+//	//	printf("Song");
+//	//	printf("zeng\n");*/  
+//	return 0;
+//}
+//int smell()
+//{
+//	printf("Smell! ");
+//}
+//int main()
+//{
+//	smell();
+//	smell();
+//	smell();
+//	printf("\n");
+//	smell();
+//	smell();
+//	printf("\n");
+//	smell();
+//	/*int tode = 10;
+//	int tode1 = 2 * tode;
+//	int tode2 = tode * tode;
+//	printf("tode=%d tode1=%dtode2=%d\n", tode, tode1, tode2);*/
+//	return 0;
+//}
+//int smell()
+//{
+//	printf("smell! smell! smell!\nsmell! smell!\nsmell!");
+//}
+//int main()
+//{
+//	smell();
+//	return 0;
+//}
+int one_three()
+{
+	puts("one");
+}
+int tew()
+{
+	puts("tew");
+}
+int main()
+{
+	puts("starting now");
+	one_three();
+	tew();
+	fputs("three",stdout);
+	printf("\ndone!\n");
+	return 0;
+}
